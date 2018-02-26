b06480087b01db9e9f409792e3a488dac2af13eb
Edlaine Zamora <ezamora@thoughtworks.com>
2018-02-25 23:07:02 -0300

    Publish

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Deploy feito!</Text>
+        <Text>Deploy done!</Text>
       </View>
     );
   }
