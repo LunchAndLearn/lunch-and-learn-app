1e167eb3c42670f5bb8468cfa1e9a95110797d48
Erika Soares da Silva <erikasons@gmail.com>
2018-04-14 22:19:44 -0300

    update the login screen to use the new LoginTextInput component.

--- src/components/loginScreen/LoginScreen.js
+++ src/components/loginScreen/LoginScreen.js
@@ -1,15 +1,34 @@
 import React, { Component } from 'react';
 import { View, Image, Text } from 'react-native';
-import User from '../user/User.js';
-import Password from '../password/Password.js';
 import NewAccount from '../newAccount/NewAccount.js';
 import LoginButton from '../loginButton/LoginButton.js';
 import Styles from './Styles.js';
+import LoginTextInput from '../loginTextInput/LoginTextInput.js';
 
 const mark = require("./logo.jpg");
 
 export default class LoginScreen extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      username: '',
+      password: ''
+    }
+  }
+
+  login = () => {
+
+  }
+
+  handleTextChange = (value, key) => {
+    this.setState({
+      [`${key}`]: value
+    })
+  }
+
   render() {
+    const { username, password } = this.state;
     return (
       <View style={Styles.container}>
           <View style={Styles.markWrap}>
@@ -17,9 +36,22 @@ export default class LoginScreen extends Component {
             <Text style={Styles.title}>Lunch & Learn</Text>
           </View>
           <View style={Styles.wrapper}>
-            <User />
-            <Password />
-            <LoginButton/>
+            <LoginTextInput 
+              iconName={'ios-person'} 
+              placeholder={'Usuário'} 
+              isSecure={false}
+              value={username}
+              changeText={(value) => this.handleTextChange(value, 'username')} 
+            />
+
+            <LoginTextInput 
+              iconName={'ios-lock'} 
+              placeholder={'Senha'} 
+              isSecure={true}
+              value={password}
+              changeText={(value) => this.handleTextChange(value, 'password')} 
+            />
+            <LoginButton onPressButton={this.login}/>
           </View>
           <NewAccount />
       </View>
