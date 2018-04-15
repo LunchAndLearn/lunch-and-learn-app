7abb65e5eccc5ef4656b89e724a13ec0ab7ed9c5
Erika Soares da Silva <erikasons@gmail.com>
2018-04-14 22:05:33 -0300

    adjust component to receive a callback function.

--- src/components/loginButton/LoginButton.js
+++ src/components/loginButton/LoginButton.js
@@ -1,24 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { View, TouchableOpacity, Text, Button, Alert } from 'react-native';
 import Styles from './Styles.js';
 
-class LoginButton extends Component {
-
-    _onPressButton() {
-        Alert.alert('Preencha o usuário e senha.')
-      }
-
-    render() {
-        return (
-            <View>
-                <TouchableOpacity activeOpacity={.5}>
-                    <View style={Styles.button}>
-                        <Text style={Styles.buttonText} onPress={this._onPressButton}>Logar</Text>   
-                    </View>
-                </TouchableOpacity>
+const LoginButton = ({ onPressButton }) => (
+    <View>
+        <TouchableOpacity activeOpacity={.5}>
+            <View style={Styles.button}>
+                <Text style={Styles.buttonText} onPress={onPressButton}>Logar</Text>   
             </View>
-        );
-    }
-}
+        </TouchableOpacity>
+    </View>
+);
 
 export default LoginButton;
