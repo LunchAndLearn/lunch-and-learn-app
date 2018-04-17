5728467e21dc1c0845cfd6022e5abd963fa39a90
Erika Soares da Silva <erikasons@gmail.com>
2018-04-17 11:32:13 -0300

    customize the icon in LoginTextInput.js

--- src/components/loginTextInput/LoginTextInput.js
+++ src/components/loginTextInput/LoginTextInput.js
@@ -6,18 +6,17 @@ import Styles from './Styles.js';
 const LoginTextInput = ({ iconName, placeholder, isSecure, value, changeText }) => (
   <View style={ Styles.inputWrap }>
     <View style={ Styles.iconWrap }>
-      <Ionicons name='ios-lock' size={20} />
+      <Ionicons name={ iconName } size={ 20 } />
     </View>
     <TextInput
       placeholderTextColor="#000"
       placeholder={ placeholder }
       style={ Styles.input }
-      onChangeText={(value) => changeText(value)}
-      value={value}
+      onChangeText={ (value) => changeText(value) }
+      value={ value }
       secureTextEntry={ isSecure }
     />
   </View>
 );
 
 export default LoginTextInput;
-
